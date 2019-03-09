125f8ec3d08afd32adeb5b0a1f32e9f8e84fc2ef
briantam23 <briantam23@gmail.com>
2019-03-09 12:59:42 -0500

    Added React Bootstrap

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,31 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Form from 'react-bootstrap/Form';
+import FormControl from 'react-bootstrap/FormControl';
+import Button from 'react-bootstrap/Button';
 
 
 class App extends Component {
     render() {
         return(
-            <hr/>
+            <Fragment>
+                <Navbar bg="dark" variant="dark" expand="lg">
+                    <Navbar.Brand href="#home">Mini e-Commerce App</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#cart">Cart</Nav.Link>
+                        <Nav.Link href="#orders">Orders</Nav.Link>
+                    </Nav>
+                    <Form inline>
+                        <FormControl type="text" placeholder="Search Products" className="mr-sm-2" />
+                        <Button variant="outline-success">Search</Button>
+                    </Form>
+                </Navbar.Collapse>
+                </Navbar>
+            </Fragment>
         )
     }
 }
